640f0fa0139ba41fb9a69be2f86b81b62fce36c5
Fazal <102220108+FrontEndProfile@users.noreply.github.com>
2022-09-18 13:33:50 +0500

    v-05

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-      AOS.init();
+AOS.init();
 
 
-      $('.navbar-toggler').click( function(){
-            $('body').toggleClass('showMenu');
-          });
+$('.navbar-toggler').click(function () {
+      $('body').toggleClass('showMenu');
+});
+
+
+
+var scene = document.getElementById('js-scene');
+var parallax = new Parallax(scene);
